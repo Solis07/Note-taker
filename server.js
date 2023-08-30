174c06fed7507ba5779d089d07511cd3162a0e16
L.J Solis <l.jsolis52@yahoo.com>
2023-08-29 22:02:43 -0500

    added an api and html folder both containing their own .js files to add my routes. I also aadded the code to connect the routes on the server.js file as well as add comments explaining lines of code.

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
+// Express
 const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Import our files containing our routes
+const api = require('./routes/API');
+const html = require('./routes/HTML');
+
+// Needed for our POST and PUT request
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Public folder
 app.use(express.static("public"));
 
+// API Route
+app.use('/api', api);
+app.use('/', html)
+
+//Used to bind and listen to the connections on the specified host and port.
 app.listen(PORT, () => console.log(`API server now on port ${PORT}!`));
